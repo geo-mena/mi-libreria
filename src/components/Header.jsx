708b2d679cb8f944226a650759ab55dfab2c1da9
Geovanny Mena <geovanny.mena@ec.seneca-corp.com>
2024-02-03 15:17:06 -0500

    Add Logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
+import { ImBooks } from "react-icons/im";
+
 export function Header() {
   return (
     <header className="mt-12 sm:mt-24 mb-8">
-      <h1 className="text-3xl sm:text-4xl font-bold font-heading text-white">Mi Libreria</h1>
+      <h1 className="text-3xl sm:text-4xl font-bold font-heading text-white">
+        <span className="flex items-center">
+          <ImBooks className="mr-2" />
+          Mi Libreía
+        </span>
+      </h1>
     </header>
   );
 }
