416f838d23429e8b67126da2e5fb141d14fa60d8
Geovanny Mena <geovanny.mena@ec.seneca-corp.com>
2024-02-05 09:41:55 -0500

    Fix Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export function Header() {
     <header className="mt-12 sm:mt-24 mb-8">
       <h1 className="text-3xl sm:text-4xl font-bold font-heading text-white">
         <span className="flex items-center">
-          <ImBooks className="mr-2" />
+          <ImBooks className="mr-2 cursor-pointer transition duration-300 ease-in-out hover:text-[#3b82f6]" />
           Mi Libreía
         </span>
       </h1>
